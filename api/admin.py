435aca8fc634c4730727dae2f2855d665b77ca2d
uezili <weslmaciel@gmail.com>
2023-02-05 11:38:49 -0300

    Register tables administrative area.

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import User, Subscription, Status, EventHistory
 # Register your models here.
+
+admin.site.register(User)
+admin.site.register(Subscription)
+admin.site.register(Status)
+admin.site.register(EventHistory)
