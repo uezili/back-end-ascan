1b8429715aba270906d633ead86e1aa1810a60c6
uezili <weslmaciel@gmail.com>
2023-02-07 23:57:02 -0300

    Modification in serializers file.

--- api/serializer.py
+++ api/serializer.py
@@ -2,25 +2,25 @@ from rest_framework import serializers
 from .models import User, Status, Subscription, EventHistory
 
 
-class UserSerializer(serializers.models):
+class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = '__all__'
 
 
-class StatusSerializer(serializers.models):
+class StatusSerializer(serializers.ModelSerializer):
     class Meta:
         model = Status
         fields = '__all__'
 
 
-class SubscriptionSerializer(serializers.models):
+class SubscriptionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Subscription
         fields = '__all__'
 
 
-class EventHistorySerializer(serializers.models):
+class EventHistorySerializer(serializers.ModelSerializer):
     class Meta:
         model = EventHistory
         fields = '__all__'
