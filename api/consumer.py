b1f989713dfab7b159f4ef9d81bf2c14d42bb6d4
uezili <weslmaciel@gmail.com>
2023-03-04 13:17:55 -0300

    Fix consumer.

--- api/consumer.py
+++ api/consumer.py
@@ -1,9 +1,6 @@
 import json
 import pika as pk
 
-# from django.core.exceptions import ObjectDoesNotExist
-# from .models import Subscription, EventHistory, Status, User
-
 
 class SubscriptionProcessor:
     def __init__(self):
@@ -22,22 +19,3 @@ class SubscriptionProcessor:
         event_type = message['event_type']
         data = message['data']
         print(event_type, data)
-
-# class SubscriptionProcessor2:
-#    def __init__(self):
-#        self._host = "rabbitmq"
-#        self._port = "5672"
-
-#    def start(self):
-#        connection2 = pk.BlockingConnection(
-#            pk.ConnectionParameters(host=self._host, port=self._port)
-#        )
-#        channel = connection2.channel()
-#        channel.queue_declare("subscriptions")
-
-#        def _callback(self, ch, method, properties, body):
-#            print(body)
-#        channel.basic_consume(queue="subscriptions",
-#                              on_message_callback=_callback, auto_ack=True)
-#        print('[*] Waiting for messages. To exit press CTRL + C.')
-#        channel.start_consuming()
