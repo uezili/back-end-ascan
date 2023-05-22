eefc364f383f05264264fa94a96d3e819da2767e
uezili <weslmaciel@gmail.com>
2023-05-21 21:06:26 -0300

    Remove connection of rabbitmq in settings file.

--- myproject/settings.py
+++ myproject/settings.py
@@ -125,5 +125,3 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-BROKER_URL = 'amqp://guest:guest@rabbitmq:5672//'
