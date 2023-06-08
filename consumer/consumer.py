e108b4b26eada063704ece80a5c9f71ce4ad44a7
uezili <weslmaciel@gmail.com>
2023-06-08 19:27:34 -0300

    Add durable in declarer queue.

--- consumer/consumer.py
+++ consumer/consumer.py
@@ -2,20 +2,18 @@ import pika
 from logging import warning
 
 
+def callback(ch, method, properties, body):
+    warning(" [x] Received %r" % body)
+
 credentials = pika.PlainCredentials("ascan", "ascan")
 connection = pika.BlockingConnection(
     pika.ConnectionParameters('rabbitmq', 5672, '/', credentials))
 channel = connection.channel()
 
-channel.queue_declare(queue='event_history')
-
-
-def callback(ch, method, properties, body):
-    warning(f" [x] Received  {body}")
-
+channel.queue_declare(queue='event_history', durable=True)
 
 channel.basic_consume(
-    'event_history', callback, auto_ack=True)
+    queue='event_history', on_message_callback=callback, auto_ack=True)
 
 warning(' [*] Waiting for messages. To exit press CTRL+C')
 channel.start_consuming()
