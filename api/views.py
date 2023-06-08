89f0b1eef2696b6db3ae5de3b148f2a3e097dbd1
uezili <weslmaciel@gmail.com>
2023-06-08 19:31:00 -0300

    Create a new view for sender endpoint.

--- api/views.py
+++ api/views.py
@@ -1,8 +1,12 @@
 from django.http import JsonResponse
 from rest_framework import viewsets
-from .models import Subscription, User, Status
+from .models import Subscription, User, Status, SendMessage
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.views import APIView
 from .serializer import SubscriptionSerializer, UserSerializer, StatusSerializer
 import pika
+import json
 
 
 class UserViewSet(viewsets.ModelViewSet):
@@ -18,3 +22,28 @@ class SubscriptionViewSet(viewsets.ModelViewSet):
 class StatusViewSet(viewsets.ModelViewSet):
     queryset = Status.objects.all()
     serializer_class = StatusSerializer
+
+class SendMessageView(APIView):
+    
+    def post(self, request):
+        body = request.data
+        if not body:
+            return Response({"error": "Field body is none."}, status=status.HTTP_400_BAD_REQUEST)
+        
+        credentials = pika.PlainCredentials("ascan", "ascan")
+        connection = pika.BlockingConnection(
+            pika.ConnectionParameters('rabbitmq', 5672, '/', credentials))
+        channel = connection.channel()
+
+        # channel.exchange_declare(exchange='event_history', exchange_type='direct')
+        channel.exchange_declare(exchange='event_history', exchange_type='fanout')
+
+        channel.queue_bind(exchange='event_history', queue='event_history')
+
+        channel.basic_publish(exchange='event_history', routing_key="", body=json.dumps(body))
+
+        connection.close()
+        print(body)
+        return Response({"success": "Message sent successfully."}, status=status.HTTP_201_CREATED)
+
+        
